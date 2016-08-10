d09df17d509cad02a8097cd6a833b37d596c6ab8
bingSz <szhebin@yahoo.com>
2016-08-10 16:28:03 +0800

    Add files via upload

---
+++ perceptron.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+	int data[8][2] = {{1, 0},
+			  {2, 0},
+			  {3, 0},
+			  {4, 0},
+			  {6, 1},
+			  {7, 1},
+			  {8, 1},
+			  {9, 1}};
+	int dataSize = sizeof(data) / sizeof(data[0]);
+	float theta;
+	int count;
+	int elementNum;
+	float error;
+	float learningRate = 0.01; 
+
+	theta = 0;
+	for (count = 0; count < 1000; count++)
+	{
+		elementNum = count % dataSize;
+		
+		error = data[elementNum][1] - (data[elementNum][0] * theta);
+		theta += learningRate * error * data[elementNum][0];
+	}
+
+	printf("%f\n", theta);
+
+	return 0;
+}
