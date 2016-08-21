503fcb32b29e6e5291e6d84a367554c44febe2db
bingSz <szhebin@yahoo.com>
2016-08-21 15:28:56 +0800

    square fitting

--- squareFitting.c
+++ squareFitting.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /*
  * How to use function squareFitting?
@@ -28,27 +29,43 @@ double doubleabs(double num)
 	}
 }
 
-squareFittingRet squareFitting(double data[][2], unsigned int dataSize, double learningRate, unsigned int maxNum, double loss)
+squareFittingRet squareFitting(double data[][2], unsigned int dataSize, double learningRate, int maxNum, double loss)
 {
 	squareFittingRet ret;
 	double a, b, c;
 	double xSquare;
 	double errorSum;
+	double allErrorSum;
 	unsigned int count;
+	unsigned int numCount;
 	unsigned int elementNum;
+	int addNum = 1;
+	int subCount;
 
 	a = 0;
 	b = 0;
 	c = 0;
 
-	for (count = 0; count < maxNum; count++)
+	if (maxNum == -1)
 	{
-		elementNum = count % dataSize;
+		addNum = 0;
+		maxNum = 1;
+	}
+	for (count = 0, numCount = 0; count < maxNum; count += addNum, numCount += 1)
+	{
+		elementNum = numCount % dataSize;
 
 		xSquare = data[elementNum][0] * data[elementNum][0];
 		errorSum = data[elementNum][1] - (a * xSquare + b * data[elementNum][0] + c);
+	
+		allErrorSum = 0;
+		for (subCount = 0; subCount < dataSize; subCount++)
+		{
+			allErrorSum += data[subCount][1] - (a * data[subCount][0] * data[subCount][0] + b * data[subCount][0] + c);
+		}
+		allErrorSum += errorSum;
 
-		if (doubleabs(errorSum) < loss)
+		if (fabs(allErrorSum) <= loss)
 		{
 			break;
 		}
@@ -69,14 +86,14 @@ squareFittingRet squareFitting(double data[][2], unsigned int dataSize, double l
 int main()
 {
 	squareFittingRet ret;
-	double data[6][2] = {{1, 9},
-                              {2, 18}, 
-                              {3, 31}, 
-                              {4, 48},
-                              {5, 69}, 
-                              {6, 94}};
-	
-	ret = squareFitting(data, 6, 0.01, 10000, 0.001);
+	double data[6][2] = {{1, 6},		/* Suppose the function is: 3x^2 + 2x + 1 */
+                              {2, 17}, 
+                              {3, 34}, 
+                              {4, 57},
+                              {5, 86}, 
+                              {6, 121}};
+
+	ret = squareFitting(data, 6, 0.001, -1, 0.0000001);
 
 	printf("%lf %lf %lf\n", ret.a, ret.b, ret.c);
 
