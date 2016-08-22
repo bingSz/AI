083d97468874dc21e4a3f43db4c416b03e2df94f
bingSz <szhebin@yahoo.com>
2016-08-22 14:51:10 +0800

    perceptron

--- perceptron.c
+++ perceptron.c
@@ -2,31 +2,34 @@
 
 int main()
 {
-	int data[8][2] = {{1, 0},
-			  {2, 0},
-			  {3, 0},
-			  {4, 0},
-			  {6, 1},
-			  {7, 1},
-			  {8, 1},
-			  {9, 1}};
+	int data[8][3] = {{-1, 1, -1},
+			  {-2, 2, -1},
+			  {-3, 3, -1},
+			  {-4, 4, -1},
+			  {1, 2, 1},
+			  {2, 3, 1},
+			  {3, 4, 1},
+			  {4, 5, 1}};
 	int dataSize = sizeof(data) / sizeof(data[0]);
-	float theta;
+	float theta[2];
 	int count;
 	int elementNum;
 	float error;
 	float learningRate = 0.01; 
 
-	theta = 0;
+	theta[0] = 0;
+	theta[1] = 0;
 	for (count = 0; count < 1000; count++)
 	{
 		elementNum = count % dataSize;
 		
-		error = data[elementNum][1] - (data[elementNum][0] * theta);
-		theta += learningRate * error * data[elementNum][0];
+		error = data[elementNum][2] - (data[elementNum][0] * theta[0]) - (data[elementNum][1] * theta[1]);
+		theta[0] += learningRate * error * data[elementNum][0];
+		theta[1] += learningRate * error * data[elementNum][1];
+	
 	}
 
-	printf("%f\n", theta);
+	printf("%f\n%f\n", theta[0], theta[1]);
 
 	return 0;
 }
