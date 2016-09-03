83ca8eba8b2172bd789fed7fd09d97b151f6112d
bingSz <szhebin@yahoo.com>
2016-09-03 17:54:12 +0800

    SVM

--- SVM/SVM.c
+++ SVM/SVM.c
@@ -23,30 +23,28 @@ int main()
 		       {2, 2}};
   int ret[4] = {-1, -1, 1, 1};
 
-  double learningRate = 0.01;
+  double learningRate = 0.001;
   double theta[2];
   double errorSum;
+  double funcRet;
   double bias;
   
   int count;
   int num;
 
   memset(theta, 0, sizeof(theta));
-  for (count = 0; count < 10000; count++)
+  for (count = 0; count < 100000; count++)
     {
       num = count % 4;
 
-      errorSum = ret[num] - vectorTimes(theta, data[num], 2) - bias;
+      funcRet = vectorTimes(theta, data[num], 2) + bias;
+      errorSum = ret[num] - funcRet;
 
-      if (errorSum <= 0 && ret[num] == 1)
+      if ((funcRet >= 1 && ret[num] == 1) || (funcRet <= -1 && ret[num] == -1))
 	{
 	  continue;
 	}
-      if (errorSum >= 0 && ret[num] == -1)
-	{
-	  continue;
-	}
-
+      
       theta[0] += errorSum * learningRate * data[num][0];
       theta[1] += errorSum * learningRate * data[num][1];
       bias += errorSum;
