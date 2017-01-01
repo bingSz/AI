ccec309fe39b59c933352890cae27fb52593939f
bingSz <szhebin@yahoo.com>
2017-01-01 17:17:41 +0800

    Add files via upload

---
+++ python/DTMF_analyser/analyse.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+
+from scipy.io import wavfile
+from numpy import *
+
+if __name__ == '__main__':
+
+    sampFreq, snd = wavfile.read('a.wav')
+
+    soundVect = []
+    for i in xrange(len(snd)):
+        soundVect.extend([snd[i]])
+
+    soundTransformed = log10(abs(fft.rfft(soundVect)))
+
+    soundIndex = linspace(0, sampFreq / 2, len(soundTransformed))
+
+    IndexSorted = list(argsort(soundTransformed))
+    IndexSorted.reverse()
+    print soundIndex[IndexSorted[:10]]
